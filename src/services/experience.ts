e2ac89e0ae284708a901816ad7d02686ad3444f9
Maurice Gift <founder@giftedtech.co.ke>
2026-05-10 02:04:27 +0300

    Update src/services/experience.ts: giftedtech.co.ke -> gifted.co.ke

--- src/services/experience.ts
+++ src/services/experience.ts
@@ -7,6 +7,6 @@ export const WORK_EXPERIENCE: WorkExperience[] = [
     duration: "2024 - Present",
     location: "Eldoret, Kenya",
     description: "Leading the development of apps for enterprise workflows. Spearheaded the integration of APIs, AI and high-performance vector databases to reduce search latency by 60%.",
-    companyImage: "https://files.giftedtech.co.ke/image/jjsScreenshot-2026-01-01-080005.png?auto=format&fit=crop&q=80&w=800"
+    companyImage: "https://files.gifted.co.ke/image/jjsScreenshot-2026-01-01-080005.png?auto=format&fit=crop&q=80&w=800"
   }
 ];
